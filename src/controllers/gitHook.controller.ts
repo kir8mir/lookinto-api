4a394ab47faf0b9073f2f6834986261246cf2c0c
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-06-28 18:49:50 +0300

    се

--- src/controllers/gitHook.controller.ts
+++ src/controllers/gitHook.controller.ts
@@ -15,17 +15,20 @@ export class gitHookController {
         console.log(`Результат выполнения команды git pull: ${stdout}`);
         // Перезагрузить сервер (замените команду на соответствующую для вашей системы)
 
-        exec('sudo npm run build', (error) => {
-          if (error) {
-            console.error(`Ошибка при сборке сервера: ${error}`);
-            // Обработка ошибки при перезагрузке сервера
-            return HttpStatus.INTERNAL_SERVER_ERROR;
-          } else {
-            console.log('Сервер успешно собран');
-            // Отправить успешный HTTP-статус
-            return HttpStatus.OK;
-          }
-        });
+        exec(
+          'sudo /root/.nvm/versions/node/v16.18.1/bin/npm run build',
+          (error) => {
+            if (error) {
+              console.error(`Ошибка при сборке сервера: ${error}`);
+              // Обработка ошибки при перезагрузке сервера
+              return HttpStatus.INTERNAL_SERVER_ERROR;
+            } else {
+              console.log('Сервер успешно собран');
+              // Отправить успешный HTTP-статус
+              return HttpStatus.OK;
+            }
+          },
+        );
 
         exec(
           'sudo /root/.nvm/versions/node/v16.18.1/bin/pm2 restart lookinto-api',
