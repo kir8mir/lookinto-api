dbec6da10e9ef339ea3d4a56eb1c329d9c220d40
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-06-28 18:41:04 +0300

    се

--- src/controllers/gitHook.controller.ts
+++ src/controllers/gitHook.controller.ts
@@ -14,6 +14,24 @@ export class gitHookController {
       } else {
         console.log(`Результат выполнения команды git pull: ${stdout}`);
         // Перезагрузить сервер (замените команду на соответствующую для вашей системы)
+
+        exec(
+          'sudo npm run build',
+          (error) => {
+            if (error) {
+              console.error(`Ошибка при сборке сервера: ${error}`);
+              // Обработка ошибки при перезагрузке сервера
+              return HttpStatus.INTERNAL_SERVER_ERROR;
+            } else {
+              console.log('Сервер успешно собран');
+              // Отправить успешный HTTP-статус
+              return HttpStatus.OK;
+            }
+          },
+        );
+      }
+    });
+
         exec(
           'sudo /root/.nvm/versions/node/v16.18.1/bin/pm2 restart lookinto-api',
           (error) => {
--- src/services/entry.service.ts
+++ src/services/entry.service.ts
@@ -13,8 +13,7 @@ export class EntryService {
     @InjectRepository(UserWordEntity)
     protected readonly userWordRepository: Repository<UserWordEntity>,
   ) {}
-
-
+  
   async globalUpdate() {
     const users = await this.userRepository.find({
       where: { status: 'ready' },
