97a2ab91d0d3fdc811e25d1630c720197431378e
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-06-28 19:05:52 +0300

    new eco

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,11 @@ module.exports = {
       name: 'lookinto-api',
       script: 'npm',
       args: 'run start:prod',
+      exec_mode: 'fork',
+      instances: 1,
+      autorestart: true,
       watch: false,
+      post_update: ['npm run build'],
     },
   ],
 };
